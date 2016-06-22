31d779e5ac065c29dd50021ff9008c5880df9ad2
eightk <huicongliu1986@gmail.com>
2016-06-21 20:43:26 -0400

    patch - added demo for AOP XML configuration instead of using the annotation in the code
    release_message:new added demo for AOP XML configuration

--- src/java/com/demo/aop/aspect/LoggingAspect.java
+++ src/java/com/demo/aop/aspect/LoggingAspect.java
@@ -5,58 +5,37 @@
  */
 package com.demo.aop.aspect;
 
-import java.lang.annotation.Annotation;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 
 /**
  *
  * @author huico
  */
-@Aspect
 public class LoggingAspect {
 
-    //add wildcard to the aspect, apply the aspect to all method starts with get and return any type, apply this aspect method to multiple methods
-    //in the get*() .. means >=0 args * means >1 args
-    @Before("allGetters() && allSquareMethods()")
     public void loggingAdvice() {
         System.out.println("All square getters advices.");
     }
 
-    //pointcut demo
-    @Before("allGetters()")
     public void secondAdvice() {
-        System.out.println("All getters advice run.");
+        System.out.println("Second advice run.");
     }
 
-    @Before("allSquareMethods()")
     public void squareAdvice(JoinPoint joinPoint) {
         System.out.println(joinPoint.getTarget()+ " method " + joinPoint.toShortString() + " Advice run.");
     }
     
-    //@After Will execute even the method fails to complete
-    //@AfterReturning will only execute when the method succeed
-    //@AfterThrowing will execute when the method throws an exception
-    //getting the return result from the method and pass it into the aspect method
-    @AfterReturning(pointcut="args(name)", returning="returnString")
-    public void stringArgMethodAdvice(String name, String returnString) {
-        System.out.println("Args Advice run after setting value: " + name + ", return string is: " + returnString);
+    public void afterReturningAdvice(String returnString) {
+        System.out.println("After Returning Advice, return string is: " + returnString);
     }
     
-    //dealing with exception
-    @AfterThrowing(pointcut="args(name)", throwing="ex")
-    public void stringArgMethodAdvice(String name, Exception ex) {
-        System.out.println("Args Advice run after setting value: " + name + ", an exception has beenthrown: " + ex.getMessage());
+    public void afterThrowingAdvice(Exception ex) {
+        System.out.println("After throwing Advice, an exception has beenthrown: " + ex.getMessage());
     }
     
     //@Around will need to use pjp.proceed() to execute the method and we can add the advice before or after the method execution.
-    @Around("@annotation(com.demo.aop.aspect.Loggable)")    
+    //@Around("@annotation(com.demo.aop.aspect.Loggable)")    
     public Object myAroundAdvice(ProceedingJoinPoint pjp) {        
         Object returnValue = null;
         try {
@@ -69,25 +48,5 @@ public class LoggingAspect {
         System.out.println("@After finally Process the method: " + pjp.toShortString());
         //can get the return object as well
         return returnValue;
-    }
-
-    //execution takes parameter about method names
-    @Pointcut("execution(public * get*(..))")
-    public void allGetters() {
-        //dummy method only for holding pointcut
-        //it will not be executed, so "All getters" will not be printed
-        System.out.println("All getters");
-    }
-
-    //within takes parameter about classes
-    @Pointcut("within(com.demo.aop.model.Square)")
-    public void allSquareMethods() {
-        //dummy method only for holding pointcut
-    }
-
-    //args takes parameter about args type
-    @Pointcut("args(String)")
-    public void allMethodsWithStringArg() {
-        //dummy method only for holding pointcut
     }
 }
